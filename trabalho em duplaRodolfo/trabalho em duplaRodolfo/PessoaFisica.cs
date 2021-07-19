5b05e7c72d56570633eacb69e79c847ac6ec870b
Rodolfo Oliveira <rodolfo.oliveira.rod@gmail.com>
2021-07-19 12:59:20 -0300

    Pessoa fisica atualizada

--- trabalho em duplaRodolfo/trabalho em duplaRodolfo/PessoaFisica.cs
+++ trabalho em duplaRodolfo/trabalho em duplaRodolfo/PessoaFisica.cs
@@ -12,5 +12,38 @@ namespace trabalho_em_duplaRodolfo
         public string Cpf { get; set; }
         public string Rg { get; set; }
         public int MyProperty { get; set; }
+
+
+        public void CadastroPf()
+        {
+            PessoaFisica cadastro = new PessoaFisica();
+            Console.WriteLine("-------Cadastrando Pessoa Fisica-------");
+            Console.WriteLine("Nome: ");
+            cadastro.Nome = Console.ReadLine();
+            Console.WriteLine("CPF:");
+            cadastro.Cpf = Console.ReadLine();
+            Console.WriteLine("RG: ");
+            cadastro.Rg = Console.ReadLine();
+            Console.WriteLine("Data de nascimento: ");
+            cadastro.DataNascimento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("-------Cadastro de endereço-------");
+            Console.WriteLine("Estado: ");
+            string estado = Console.ReadLine();
+            Console.WriteLine("Cidade:");
+            string cidade = Console.ReadLine();
+            Console.WriteLine("Bairro: ");
+            string bairro = Console.ReadLine();
+            Console.WriteLine("Rua: ");
+            string rua = Console.ReadLine();
+            Console.WriteLine("Número:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Complemento: ");
+            string complemento = Console.ReadLine();
+            Endereco = new Endereco(estado, cidade, bairro, rua, num, complemento);
+            Menu.Pf.Add(cadastro);
+
+
+
+        }
     }
 }
