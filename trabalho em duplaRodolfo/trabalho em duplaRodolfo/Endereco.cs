31f91f317cff840a914f31bf354659e708ce8c7a
rafael zancanro <rafa10_zc@hotmail.com>
2021-07-19 11:02:13 -0300

    passei

---
+++ trabalho em duplaRodolfo/trabalho em duplaRodolfo/Endereco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_em_duplaRodolfo
+{
+    class Endereco
+    {
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public int Num { get; set; }
+        public string Complemento { get; set; }
+        public Endereco(string estado, string cidade, string bairro, string rua, int num, string complemento)
+        {
+            Estado = estado;
+            Cidade = cidade;
+            Bairro = bairro;
+            Rua = rua;
+            Num = num;
+            Complemento = complemento;
+        }
+    }
+}
